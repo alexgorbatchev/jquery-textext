0b7b9d826631d2e475db48e4b2e602cef0669abf
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2011-08-17 20:11:00 +0200

    Completed the doc for AJAX and updated it for the new doc_* tags.

--- src/textext.plugin.ajax.js
+++ src/textext.plugin.ajax.js
@@ -1,7 +1,14 @@
 (function($)
 {
 	/**
-	 * 
+	 * ### About
+	 *
+	 * AJAX component is very useful if you want to load list of items from a data point and pass it
+	 * to the Autocomplete or Filter components.
+	 *
+	 * Because it meant to be as a helper method for either Autocomplete or Filter components, without
+	 * either of these two present AJAX component won't do anything.
+	 *
 	 * @author agorbatchev
 	 * @date 2011/08/16
 	 * @id TextExtAjax
@@ -27,8 +34,6 @@
 		 */
 
 		/**
-		 * #### ajax.data.callback
-		 *
 		 * By default, when user starts typing into the text input, AJAX plugin will start making requests
 		 * to the `url` that you have specified and will pass whatever user has typed so far as a parameter
 		 * named `q`, eg `?q=foo`.
@@ -42,15 +47,15 @@
 		 *         return { 'search' : query };
 		 *     } 
 		 *
+		 * @name ajax.data.callback
+		 * @default null
 		 * @author agorbatchev
 		 * @date 2011/08/16
 		 * @id TextExtAjax.options.data.callback
 		 */
-		OPT_DATA_CALLBACK   = 'ajax.data.callback',
+		OPT_DATA_CALLBACK = 'ajax.data.callback',
 		
 		/**
-		 * #### ajax.cache.results
-		 *
 		 * By default, the server end point is constantly being reloaded whenever user changes the value
 		 * in the text input. If you'd rather have the client do result filtering, you can return all
 		 * possible results from the server and cache them on the client by setting this option to `true`.
@@ -58,38 +63,106 @@
 		 * In such a case, only one call to the server will be made and filtering will be performed on
 		 * the client side using `ItemManager` attached to the core.
 		 *
+		 * @name ajax.data.results
+		 * @default false
 		 * @author agorbatchev
 		 * @date 2011/08/16
 		 * @id TextExtAjax.options.cache.results
 		 */
-		OPT_CACHE_RESULTS   = 'ajax.cache.results',
+		OPT_CACHE_RESULTS = 'ajax.cache.results',
 		
 		/**
-		 * #### ajax.loading.delay
-		 *
 		 * The loading message delay is set in seconds and will specify how long it would take before
 		 * user sees the message. If you don't want user to ever see this message, set the option value
 		 * to `Number.MAX_VALUE`.
 		 *
+		 * @name ajax.loading.delay
+		 * @default 0.5
 		 * @author agorbatchev
 		 * @date 2011/08/16
 		 * @id TextExtAjax.options.loading.delay
 		 */
-		OPT_LOADING_DELAY   = 'ajax.loading.delay',
+		OPT_LOADING_DELAY = 'ajax.loading.delay',
+
+		/**
+		 * Whenever an AJAX request is made and the server takes more than the number of seconds specified
+		 * in `ajax.loading.delay` to respond, the message specified in this option will appear in the drop
+		 * down.
+		 *
+		 * @name ajax.loading.message
+		 * @default "Loading..."
+		 * @author agorbatchev
+		 * @date 2011/08/17
+		 * @id TextExtAjax.options.loading.message
+		 */
 		OPT_LOADING_MESSAGE = 'ajax.loading.message',
-		OPT_TYPE_DELAY      = 'ajax.type.delay',
+
+		/**
+		 * When user is typing in or otherwise changing the value of the text input, it's undesirable to make
+		 * an AJAX request for every keystroke. Instead it's more conservative to send a request every number
+		 * of seconds while user is typing the value. This number of seconds is specified by the `ajax.type.delay`
+		 * option.
+		 *
+		 * @name ajax.type.delay
+		 * @default 0.5
+		 * @author agorbatchev
+		 * @date 2011/08/17
+		 * @id TextExtAjax.options.type.delay
+		 */
+		OPT_TYPE_DELAY = 'ajax.type.delay',
+
+		/**
+		 * ### Events
+		 *
+		 * AJAX component dispatches or reacts to the following events.
+		 *
+		 * @author agorbatchev
+		 * @date 2011/08/17
+		 * @id TextExtAjax.events
+		 */
+
+		/**
+		 * AJAX component reacts to the `getSuggestions` event dispatched by the Autocomplete plugin.
+		 *
+		 * @name getSuggestions
+		 * @author agorbatchev
+		 * @date 2011/08/17
+		 * @id TextExtAjax.events.getSuggestions
+		 */
+
+		/**
+		 * In the event of successful AJAX request, the AJAX coponent dispatches the `setSuggestions`
+		 * event meant to be recieved by the Autocomplete plugin.
+		 *
+		 * @name setSuggestions
+		 * @author agorbatchev
+		 * @date 2011/08/17
+		 * @id TextExtAjax.events.setSuggestions
+		 */
+		EVENT_SET_SUGGESTION = 'setSuggestions',
 
 		DEFAULT_OPTS = {
 			ajax : {
-				typeDelay : 0.5,
+				typeDelay      : 0.5,
 				loadingMessage : 'Loading...',
-				loadingDelay : 0.5,
-				cacheResults : false,
-				dataCallback : null
+				loadingDelay   : 0.5,
+				cacheResults   : false,
+				dataCallback   : null
 			}
 		}
 		;
 
+	/**
+	 * Initialization method called by the core during plugin instantiation.
+	 *
+	 * @signature TextExtAjax.init(parent)
+	 *
+	 * @param parent {TextExt} Instance of the TextExt core class.
+	 *
+	 * @author agorbatchev
+	 * @date 2011/08/17
+	 * @id TextExtAjax.init
+	 */
 	p.init = function(parent)
 	{
 		var self = this;
@@ -108,11 +181,14 @@
 	/**
 	 * Performas an async AJAX with specified options.
 	 *
+	 * @signature TextExtAjax.load(query)
+	 *
 	 * @param query {String} Value that user has typed into the text area which is
 	 * presumably the query.
 	 *
 	 * @author agorbatchev
 	 * @date 2011/08/14
+	 * @id TextExtAjax.load
 	 */
 	p.load = function(query)
 	{
@@ -137,8 +213,16 @@
 	 * Successful call AJAX handler. Takes the data that came back from AJAX and the
 	 * original query that was used to make the call.
 	 *
+	 * @signature TextExtAjax.onComplete(data, query)
+	 *
+	 * @param data {Object} Data loaded from the server, should be an Array of strings
+	 * by default or whatever data structure your custom `ItemManager` implements.
+	 *
+	 * @param query {String} Query string, ie whatever user has typed in.
+	 *
 	 * @author agorbatchev
 	 * @date 2011/08/14
+	 * @id TextExtAjax.onComplete
 	 */
 	p.onComplete = function(data, query)
 	{
@@ -158,15 +242,18 @@
 			result = self.itemManager().filter(data, query);
 		}
 
-		self.trigger('setSuggestions', { result : result });
+		self.trigger(EVENT_SET_SUGGESTION, { result : result });
 	};
 
 	/**
 	 * If show loading message timer was started, calling this function disables it,
 	 * otherwise nothing else happens.
 	 *
+	 * @signature TextExtAjax.dontShowLoading()
+	 *
 	 * @author agorbatchev
 	 * @date 2011/08/16
+	 * @id TextExtAjax.dontShowLoading
 	 */
 	p.dontShowLoading = function()
 	{
@@ -174,11 +261,14 @@
 	};
 
 	/**
-	 * Shows message specified in `ajaxLoadingMessage` if loading data takes more than
-	 * number of seconds specified in `ajaxLoadingDelay`.
+	 * Shows message specified in `ajax.loading.message` if loading data takes more than
+	 * number of seconds specified in `ajax.loading.delay`.
+	 *
+	 * @signature TextExtAjax.showLoading()
 	 *
 	 * @author agorbatchev
 	 * @date 2011/08/15
+	 * @id TextExtAjax.showLoading
 	 */
 	p.showLoading = function()
 	{
@@ -201,11 +291,18 @@
 
 	/**
 	 * Reacts to the `getSuggestions` event and begin loading suggestions. If
-	 * `ajaxCacheResults` is specified, all calls after the first one will use
+	 * `ajax.cache.results` is specified, all calls after the first one will use
 	 * cached data and filter it with the `core.itemManager.filter()`.
+	 *
+	 * @signature TextExtAjax.onGetSuggestions(e, data)
+	 *
+	 * @param e {Object} jQuery event.
+	 * @param data {Object} Data structure passed with the `getSuggestions` event
+	 * which contains the user query, eg `{ query : "..." }`.
 	 *
 	 * @author agorbatchev
 	 * @date 2011/08/15
+	 * @id TextExtAjax.onGetSuggestions
 	 */
 	p.onGetSuggestions = function(e, data)
 	{
