915da4e52544cc03ac9425c1f8ba6ae64686862e
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2012-01-14 15:21:35 -0800

    Reved version to 1.2.0

--- src/js/textext.plugin.filter.js
+++ src/js/textext.plugin.filter.js
@@ -2,7 +2,7 @@
  * jQuery TextExt Plugin
  * http://alexgorbatchev.com/textext
  *
- * @version 1.1.0
+ * @version 1.2.0
  * @copyright Copyright (C) 2011 Alex Gorbatchev. All rights reserved.
  * @license MIT License
  */
